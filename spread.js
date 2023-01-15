336b007204aaae7b139f9e7b42602f0238c88af2
Neelchaudhari98 <122378083+Neelchaudhari98@users.noreply.github.com>
2023-01-15 17:13:32 -0500

    Add files via upload

---
+++ spread.js
@@ -0,0 +1,9 @@
+function userTopFriends(firstFriend, secondFriend, thirdFriends) {
+        console.log(firstFriend);
+        console.log(secondFriend);
+        console.log(thirdFriends);
+     }
+     
+     userTopFriends(...['Alice', 'Bob', 'Michelle']);
+     
+    
